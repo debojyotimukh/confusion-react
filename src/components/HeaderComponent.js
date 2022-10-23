fec958ad22e9b9a90c18413ee709e958e79f4c38
Debojyoti Mukherjee <debojyoti.mukherjee@capgemini.com>
2022-10-23 23:14:08 +0530

    delete duplicate logo file

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -107,7 +107,7 @@ class Header extends Component {
           <NavbarToggler onClick={this.toggleNav} />
           <NavbarBrand className="mr-auto" href="/">
             <img
-              src="assets/images/logo.png"
+              src="logo.png"
               height="30"
               width="41"
               alt="Ristorante Con Fusion"
