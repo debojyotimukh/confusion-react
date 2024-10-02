bf1c24d1d18e56f1c52c4f1496e740c28a624702
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2024-10-03 00:53:55 +0530

    React18-migration: Add page transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Footer from "./modules/common/Footer";
 import Header from "./modules/common/Header";
@@ -7,21 +8,25 @@ import About from "./modules/about/About";
 import Menu from "./modules/menu/Menu";
 import DishDetail from "./modules/menu/DishDetail";
 import Contact from "./modules/contact/Contact";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const App = () => {
+  const location = useLocation();
   return (
-    <BrowserRouter>
+    <TransitionGroup>
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/home" element={<Home />} />
-        <Route exact path="/aboutus" element={<About />} />
-        <Route exact path="/menu" element={<Menu />} />
-        <Route exact path="/menu/:dishId" element={<DishDetail />} />
-        <Route exact path="/contactus" element={<Contact />} />
-      </Routes>
+      <CSSTransition key={location.key} classNames="page" timeout={300}>
+        <Routes location={location}>
+          <Route exact path="*" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/aboutus" element={<About />} />
+          <Route exact path="/menu" element={<Menu />} />
+          <Route exact path="/menu/:dishId" element={<DishDetail />} />
+          <Route exact path="/contactus" element={<Contact />} />
+        </Routes>
+      </CSSTransition>
       <Footer />
-    </BrowserRouter>
+    </TransitionGroup>
   );
 };
 
